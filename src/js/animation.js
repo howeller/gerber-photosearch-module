47b391d27a2f7fd8f23707f7a2c7e1295bf43968
Eric Howell <eric.howell@hogarthww.com>
2022-03-19 00:45:25 -0400

    Checking in 'Enter Today' button dev on mobile and tablet sizes.

--- src/js/animation.js
+++ src/js/animation.js
@@ -9,6 +9,7 @@
 	function init(e){
 		cl('init');
 		windowWidth = window.innerWidth;
+		gsap.set('.hero',{visibility:'visible'});
 		startAnimation(windowWidth);
 	}
 
@@ -59,14 +60,17 @@
 		tl
 			.fromTo('#wave_tab', { y:'100%'},{y:0, duration:.5})
 			// .fromTo('#wave_tab', { x:'-50%', y:'100%'},{y:0})
-			.fromTo('#logo', { scale:0, x:0, y:'50%' }, { scale:1.1, transformOrigin:'50% 50%', duration:1, ease:'back.out(1.3)'})
+			.fromTo('#logo', { scale:0, x:0, y:'50%' }, { scale:1.4, transformOrigin:'50% 50%', duration:1, ease:'back.out(1.3)'})
 			.add('frame2','+=1')
 			.to('#logo', { scale: 1, y:0, duration: 1/*, marginTop:'1.5rem'*/}, 'frame2')
 			.to('#wave_tab', {  x:'-50%', y:'45%', duration: 1}, 'frame2')
 			// .to('#wave_tab', { x:0, y:'50%', duration: 1}, 'frame2')
-			.fromTo('#book', { scale:0, x:0, y:0 }, { scale:1, transformOrigin:'50% 50%', ease:'back.out(1.3)'}, '-=.5')
-			.fromTo('#txt', {scale:0},{ scale:1, transformOrigin:'50% 50%', duration:1, ease:'back.out(1.3)'})
-			// .seek('frame2')
+			// .fromTo('#book', { scale:0, x:0, y:0 }, { scale:1, transformOrigin:'50% 50%', ease:'back.out(1.3)'}, '-=.5')
+			.add(popInTl('#book'))
+			.add(spinInTl('#enter'))
+			.add(popInTl('#txt'))
+			// .fromTo('#txt', {scale:0},{ scale:1, transformOrigin:'50% 50%', duration:1, ease:'back.out(1.3)'})
+			.seek('frame2')
 			// tl.pause(.6);
 	}
 
@@ -80,11 +84,24 @@
 			.fromTo('#logo', { scale:0, x:0, y:0 }, { scale:1, transformOrigin:'50% 50%', duration:1, ease:'back.out(1.3)'})
 			.add('frame2','+=.1')
 			.to('#wave_m', {  x:'-50%', y:'45%', duration: 1}, 'frame2')
-			.fromTo('#book', { scale:0, x:0, y:0 }, { scale:1, transformOrigin:'50% 50%', ease:'back.out(1.3)'}, '-=.5')
-			.fromTo('#logo-wrapper', {y:0}, { y:'-10%', duration: 1}, '')
-			.fromTo('#txt', {scale:0},{ scale:1, transformOrigin:'50% 50%', duration:1, ease:'back.out(1.3)'})
-			// .seek('frame2')
+			// .fromTo('#book', { scale:0, x:0, y:0 }, { scale:1, transformOrigin:'50% 50%', ease:'back.out(1.3)'}, '-=.5')
+			.add(popInTl('#book'))
+			// .add(spinInTl('#enter'))
+			.fromTo('#enter', { scale:0, y:0, rotation:180 }, { rotation:-5, scale:1, transformOrigin:'50% 50%', ease:'back.out(1.3)', duration:0.8}, '-=.5')
+			.fromTo('#logo-wrapper', {y:0}, { y:'-10%', duration: 1})
+			.add(popInTl('#txt'))
+			// .fromTo('#txt', {scale:0},{ scale:1, transformOrigin:'50% 50%', duration:1, ease:'back.out(1.3)'})
+			.seek('frame2')
 			// tl.pause(.6);
+	}
+
+	function popInTl(elId, speed=0.5){
+		return gsap.timeline()
+			.fromTo(elId, { scale:0, y:0 }, { duration:speed, scale:1, transformOrigin:'50% 50%', ease:'back.out(1.3)'});
+	}
+	function spinInTl(elId, speed=0.8){
+		return gsap.timeline()
+			.fromTo(elId, { scale:0, rotation:180 }, { duration:speed, rotation:0, scale:1, transformOrigin:'50% 50%', ease:'back.out(1.3)'});
 	}
 
 	function getCenterX(elId){
