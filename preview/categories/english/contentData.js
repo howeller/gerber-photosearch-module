75abcd6150434e4d04a9bd8eff2f3133871b7016
Eric Howell <eric.howell@hogarthww.com>
2022-03-21 12:31:50 -0700

    Created static pages for preview site.

--- preview/categories/english/contentData.js
+++ preview/categories/english/contentData.js
@@ -9,7 +9,7 @@ var objData = {
 				title: "",
 				bannerList: [
 					{
-						overwriteSizeDisplay: "-",
+						overwriteSizeDisplay: "Mobile",
 						label: "Homepage Module",
 						src: "Gerber_Responsive_Homepage_Banner",
 						w: 780,
@@ -21,8 +21,58 @@ var objData = {
 						sizesArray: "[" + 
 							"Mobile:780x1438,"+
 							"Tablet Landscape:1024x650,"+
+							"Desktop:1440x400,"+
+							// "Tablet Portrait:720x1024,"+
+							// "Apple iPad Pro:512x683,"+
+							// "Apple iPhone 11 Pro:338x731," +
+							// "Apple iPhone 8 Plus:365x648," +
+							// "Apple iPhone XR:318x689," + 
+							// "Apple iPhone SE:314x559," +
+							// "Google Pixel:335x595," +
+							// "Samsung Galaxy 8:324x666," +
+							// "fullscreen" + 
+						"]"
+					},
+					{
+						overwriteSizeDisplay: "Tablet",
+						label: "Homepage Module",
+						src: "Gerber_Responsive_Homepage_Banner",
+						w: 1024,
+						h: 650,
+						type: "html5responsive",
+						approved: false,
+						dload: true,
+						backup: false,
+						sizesArray: "[" + 
+							"Tablet Landscape:1024x650,"+
+							"Mobile:780x1438,"+
 							// "Tablet Portrait:720x1024,"+
 							"Desktop:1440x400,"+
+							// "Apple iPad Pro:512x683,"+
+							// "Apple iPhone 11 Pro:338x731," +
+							// "Apple iPhone 8 Plus:365x648," +
+							// "Apple iPhone XR:318x689," + 
+							// "Apple iPhone SE:314x559," +
+							// "Google Pixel:335x595," +
+							// "Samsung Galaxy 8:324x666," +
+							// "fullscreen" + 
+						"]"
+					},
+					{
+						overwriteSizeDisplay: "Desktop",
+						label: "Homepage Module",
+						src: "Gerber_Responsive_Homepage_Banner",
+						w: 1440,
+						h: 400,
+						type: "html5responsive",
+						approved: false,
+						dload: true,
+						backup: false,
+						sizesArray: "[" + 
+							"Desktop:1440x400,"+
+							"Mobile:780x1438,"+
+							"Tablet Landscape:1024x650,"+
+							// "Tablet Portrait:720x1024,"+
 							// "Apple iPad Pro:512x683,"+
 							// "Apple iPhone 11 Pro:338x731," +
 							// "Apple iPhone 8 Plus:365x648," +
