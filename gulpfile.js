ec7edebe3a2f153df472d1f717ee1e69f26e579d
Eric Howell <eric.howell@hogarthww.com>
2022-03-30 13:50:21 -0700

    Added gulp-if + gulp-sourcemaps to gulp tasks. New "prod" tasks to uglify  & create sourcemaps for the compiled js and css.

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,11 @@ const del = require('del'),
 	merge = require('merge-stream'),
 	path = require('path'),
 	gulp = require('gulp'),
+	gulpif = require('gulp-if'),
 	sass = require('gulp-sass')(require('sass')),
 	gcmq = require('gulp-group-css-media-queries'),
 	autoprefixer = require('gulp-autoprefixer'),
+	sourcemaps = require('gulp-sourcemaps'),
 	rename = require('gulp-rename'),
 	uglify = require('gulp-uglify'),
 	uglifycss = require('gulp-uglifycss');
@@ -23,25 +25,27 @@ const dir = {
 const prefixerPrefs = {
 	cascade: false
 }
-// const { dist, html, js, scss, images  } = dir;
 
-// gulp.task('sass')	
-function buildStyles() {
-  return gulp.src(dir.scss+'style.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer(prefixerPrefs))
-    .pipe(gcmq())
-    .pipe(gulp.dest(dir.dist));
+function buildStyles(isProduction=false) {
+	return gulp.src(dir.scss+'style.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer(prefixerPrefs))
+		.pipe(gcmq())
+		.pipe(gulpif(isProduction, uglifycss()))
+		.pipe(sourcemaps.write('./maps'))
+		.pipe(gulp.dest(dir.dist));
 }
 
-function build() {
+function build(isProduction=false) {
 
 	let _html = gulp.src(dir.html+'*.html')
 		.pipe(rename('index.html'));
 
-	let _js = gulp.src( dir.js+'**')
-	// let _images = gulp.src(dir.images+'**')
-	// 	.pipe(gulp.dest(dir.dist+'images/'));
+	let _js = gulp.src(dir.js+'**', '!'+dir.js+'*.min.js')
+		.pipe(gulpif(isProduction, sourcemaps.init()))
+		.pipe(gulpif(isProduction, uglify()))
+		.pipe(gulpif(isProduction, sourcemaps.write('./maps')));
 
 	return merge(_html, _js).pipe(gulp.dest(dir.dist));
 }
@@ -49,20 +53,15 @@ function build() {
 function images(){
 	return gulp.src(dir.images+'**').pipe(gulp.dest(dir.dist+'images/'));
 }
-function minifyJs(){
-	return gulp.src(dir.js+'**', '!'+dir.js+'*.min.js')
-		.pipe(uglify())
-		// .pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest(dir.dist))
-};
 
 gulp.task('clean', () => { return del(dir.dist+'**/*'); });
-gulp.task('build', build);
-gulp.task('sass', buildStyles);
-gulp.task('min', gulp.series('build', minifyJs));
-gulp.task('images', gulp.series('clean', images, 'sass', 'build'));
-gulp.task('default', gulp.series('sass', 'build'));
-// gulp.task('watch', function(callback) { return gulp.watch(dir.src+'**/**', gulp.series('default')); callback(); });
+gulp.task('build:dev', () => { return build(false)});
+gulp.task('build:prod', () => { return build(true)});
+gulp.task('sass:dev', () => { return buildStyles(false)});
+gulp.task('sass:prod', () => { return buildStyles(true)});
+gulp.task('prod', gulp.series('sass:prod', 'build:prod'));
+gulp.task('images', gulp.series('clean', images, 'sass:dev', 'build:dev'));
+gulp.task('default', gulp.series('sass:dev', 'build:dev'));
 gulp.task('watch', () => { return gulp.watch(['src/*/**'], gulp.series('default'))});
 
 
