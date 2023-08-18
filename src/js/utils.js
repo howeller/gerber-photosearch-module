aeffc544321797486415665a89d4cd0e84661c57
Eric Howell <eric.howell@hogarthww.com>
2023-08-18 15:28:54 -0700

    stash

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,4 +1,15 @@
-	const cl = txt => console.log('%c '+txt,'background: rgba(51, 255, 0, 0.3); color: white;');
-	const id = txt => document.getElementById(txt);
+const cl = txt => console.log('%c '+txt,'background: rgba(51, 255, 0, 0.3); color: white;');
 
-	export { cl, id };
+const id = txt => document.getElementById(txt);
+
+function debounce(func, delay = 250) {
+	let timerId;
+	return (...args) => {
+		clearTimeout(timerId);
+		timerId = setTimeout(() => {
+			func.apply(this, args);
+		}, delay);
+	};
+}
+
+export { cl, id, debounce };
